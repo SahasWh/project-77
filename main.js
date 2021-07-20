0d81d21f68e4d631f06d1a566ebb129004c41052
SahasWh <82063178+SahasWh@users.noreply.github.com>
2021-07-20 13:34:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+        var first_name = "sahas";
+        console.log(first_name);
+        
+        var last_name = "bairu";
+        cosnole.log(last_name);
+
+        var full_name = first_name.concat(last_name);
+        console.log = (full_name);
+
+        var a = 10;
+        console.log = (a);
+
+        var b = 5;
+        console.log = (b);
+
+        console.log(a + b);
